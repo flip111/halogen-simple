64f00b4a61a397addc44591e46f30e0c80b56a1a
flip111 <flip101@gmail.com>
2025-04-25 19:17:51 +0200

    Stylistic changes

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Simple Purescript Halogen demo app
 
-Useful as template to quickly try things with halogen.
+Useful as template to quickly trying things with halogen.
 
 Based on https://github.com/purescript-halogen/purescript-halogen/tree/master/examples/basic
 
--- src/Main.purs
+++ src/Main.purs
@@ -16,25 +16,27 @@ main = HA.runHalogenAff do
   body <- HA.awaitBody
   runUI component unit body
 
-type State = { enabled :: Boolean }
-
-data Action = Toggle
-
-component :: forall q i o m. H.Component q i o m
-component = H.mkComponent
-  { initialState: initialState
-  , render: render
-  , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+type State =
+  { enabled :: Boolean
   }
 
 initialState :: forall i. i -> State
 initialState _ = { enabled: false }
 
-render :: forall m. State -> H.ComponentHTML Action () m
-render state =
-  let label = if state.enabled then "On" else "Off"
-  in HH.button [ HP.title label, HE.onClick \_ -> Toggle] [HH.text label]
+component :: forall q i o m. H.Component q i o m
+component = H.mkComponent
+  { initialState: initialState
+  , render: render
+  , eval: H.mkEval $ H.defaultEval {handleAction = handleAction}
+  }
+
+data Action = Toggle
 
 handleAction :: forall o m. Action -> H.HalogenM State Action () o m Unit
 handleAction = case _ of
-  Toggle -> H.modify_ \st -> st { enabled = not st.enabled }
+  Toggle -> H.modify_ \st -> st {enabled = not st.enabled}
+
+render :: forall m. State -> H.ComponentHTML Action () m
+render s =
+  let label = if s.enabled then "On" else "Off"
+  in HH.button [ HP.title label, HE.onClick \_ -> Toggle] [HH.text label]
